2b65b89c5e361a64fc436cc715db17772f73bd7e
AHShadkam <AmirHosein.Shadkam@searshc.com>
2016-08-11 17:39:59 -0500

    Update Address_line2_dataExtract.sql

--- Address_line2_dataExtract.sql
+++ Address_line2_dataExtract.sql
@@ -97,3 +97,21 @@ AND dbo.CountWords(Address_Line2) = 4
 AND dbo.ReturnStringPart(Address_Line2,4) IN (SELECT Street_Suffix FROM AD_Customer.dbo.Street_Suffix_list)
 AND dbo.ReturnStringPart(Address_Line2,2) NOT IN ('e','e.','east','w','w.','west','n','n.','no.','north','s','s.','so.','south');
 GO
+
+
+/** 5 words, street suffix (3), 'APT' (4) **/
+UPDATE AD_Customer.dbo.NPJXTCN_GOLDEN 
+SET
+	Address_number=dbo.ReturnStringPart(Address_Line2,1),
+	Address_Street_Name=dbo.ReturnStringPart(Address_Line2,2),
+	Address_Street_Suffix=(SELECT UPPER(T2.Abbreviation) 
+	                        FROM AD_Customer.dbo.Street_Suffix_list T2
+				WHERE dbo.ReturnStringPart(Address_Line2,3) = T2.Street_Suffix
+				),
+	Address_UnitNumber=dbo.ReturnStringPart(Address_Line2,5)
+WHERE len(address_line2) != 0
+AND ISNUMERIC(dbo.ReturnStringPart(Address_Line2,1)) = 1
+AND dbo.CountWords(Address_Line2) = 5
+AND dbo.ReturnStringPart(Address_Line2,3) IN (SELECT Street_Suffix FROM AD_Customer.dbo.Street_Suffix_list)
+AND dbo.ReturnStringPart(Address_Line2,4) IN ('APT','APT.');
+GO
