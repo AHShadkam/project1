b5785652f87a518c821a14f63e4e0b1daeda4d9e
AHShadkam <AmirHosein.Shadkam@searshc.com>
2016-08-12 11:21:46 -0500

    Update clean_up_2.sql

--- clean_up_2.sql
+++ clean_up_2.sql
@@ -32,6 +32,24 @@ Begin
 End;
 GO
 
+
+IF OBJECT_ID('[dbo].[RemoveLeadTrailComma]') IS NOT NULL
+   DROP FUNCTION [dbo].[RemoveLeadTrailComma];
+GO
+Create Function [dbo].[RemoveLeadTrailComma](@Temp VarChar(100))
+Returns VarChar(100)
+AS
+Begin
+
+    IF LEFT(@Temp,1)= ','
+	SET @Temp=Stuff(@Temp,1, 1, '')
+    IF RIGHT(@Temp,1)=','
+	SET @Temp=Stuff(@Temp,LEN(@TEMP),1, '')
+		
+    Return @Temp
+End;
+GO
+
 update [AD_Customer].[dbo].[NPJXTCN_GOLDEN] 
 set [Address_Street_Name] = dbo.RemoveNumberSign([Address_Street_Name])
 WHERE [Address_Street_Name] LIKE '%#%';
@@ -40,4 +58,9 @@ GO
 update [AD_Customer].[dbo].[NPJXTCN_GOLDEN] 
 SET [Address_Street_Name] = dbo.RemoveLeadTrailDash([Address_Street_Name])
 WHERE [Address_Street_Name] LIKE '%-%';
+GO
+
+update [AD_Customer].[dbo].[NPJXTCN_GOLDEN] 
+SET [Address_Street_Name] = dbo.RemoveLeadTrailComma([Address_Street_Name])
+WHERE [Address_Street_Name] LIKE '%,%';
 GO
