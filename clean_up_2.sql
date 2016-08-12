3f62dcaeeadfbbaafc527461fd67464534b932ec
AHShadkam <AmirHosein.Shadkam@searshc.com>
2016-08-12 11:16:20 -0500

    Update clean_up_2.sql

--- clean_up_2.sql
+++ clean_up_2.sql
@@ -15,5 +15,29 @@ Begin
 End;
 GO
 
-update [AD_Customer].[dbo].[NPJXTCN_GOLDEN] set [Address_Street_Name] = dbo.RemoveNumberSign([Address_Street_Name]);
+IF OBJECT_ID('[dbo].[RemoveLeadTrailDash]') IS NOT NULL
+   DROP FUNCTION [dbo].[RemoveLeadTrailDash];
+GO
+Create Function [dbo].[RemoveLeadTrailDash](@Temp VarChar(100))
+Returns VarChar(100)
+AS
+Begin
+
+   IF LEFT(@Temp,1)= '-'
+		SET @Temp=Stuff(@Temp,1, 1, '')
+   IF RIGHT(@Temp,1)='-'
+		SET @Temp=Stuff(@Temp,LEN(@TEMP),1, '')
+		
+    Return @Temp
+End;
+GO
+
+update [AD_Customer].[dbo].[NPJXTCN_GOLDEN] 
+set [Address_Street_Name] = dbo.RemoveNumberSign([Address_Street_Name])
+WHERE [Address_Street_Name] LIKE '%#%';
+GO
+
+update [AD_Customer].[dbo].[NPJXTCN_GOLDEN] 
+SET [Address_Street_Name] = dbo.RemoveLeadTrailDash([Address_Street_Name])
+WHERE [Address_Street_Name] LIKE '%-%';
 GO
