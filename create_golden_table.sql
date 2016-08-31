281632b0e81d3931652f99f5786643ac390065f0
AHShadkam <AmirHosein.Shadkam@searshc.com>
2016-08-31 11:49:45 -0500

    Update create_golden_table.sql

--- create_golden_table.sql
+++ create_golden_table.sql
@@ -18,8 +18,12 @@ CREATE TABLE [AD_Customer].[dbo].[NPJXTCN_GOLDEN]
       Address_BoxNubmer varchar(10),
       Address_UnitNumber varchar(6),
       Address_Line2 varchar(30),
+      Address_Town varchar(30),
       Address_ZIP_1 varchar(6),
       Address_ZIP_2   varchar(6),
+      CN_date_effect datetime2,
+      CN_last_name_chg_dt datetime2,
+      CN_purge_dt datetime2,
       Email_Primary varchar(50),
       Email_Secondary varchar(50),
       Email_Ins_ts datetime2,
@@ -47,8 +51,12 @@ INSERT INTO [AD_Customer].[dbo].[NPJXTCN_GOLDEN]
       ,Address_BoxNubmer
       ,Address_UnitNumber
       ,Address_Line2
+      ,Address_Town
       ,Address_ZIP_1
       ,Address_ZIP_2
+      ,CN_date_effect 
+      ,CN_last_name_chg_dt 
+      ,CN_purge_dt
       ,Email_Primary
 --    ,Email_Secondary
       ,Email_Ins_ts
@@ -68,10 +76,13 @@ SELECT  [cn_cust_key]
         ,[cn_box_no]
         ,[cn_unit_no]
         ,[cn_addr_line_2]
+        ,[cn_town]
         ,[cn_zip_pc]
         ,[cn_zip_sector_seg]
+        ,CONVERT(datetime2,[cn_date_effect])
+        ,CONVERT(datetime2,[lst_nm_chg_dt])
+        ,CONVERT(datetime2,[purge_dt])
         ,UPPER([CUS_EML_AD])
---        ,[CRT_TS]
         ,CONVERT(datetime2,[CRT_TS])
         ,[cn_tel_no]
 FROM  [AD_Customer].[dbo].[NPJXTCN_RAO] ;
