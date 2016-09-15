bb1b3a3f64cf63700ce5309e1896e431ae92992a
AHShadkam <AmirHosein.Shadkam@searshc.com>
2016-09-15 12:28:35 -0500

    Create house_holding.sql

---
+++ house_holding.sql
@@ -0,0 +1,19 @@
+WITH HH AS(
+SELECT
+  Cust_no
+  ,MIN(Cust_no) OVER (PARTITION BY Name_last+ Address_number+ Address_Zip_1) AS HH_ID
+  ,Name_last+Address_number+Address_Zip_1 AS combination
+FROM [AD_Customer].[dbo].NPJXTCN_GOLDEN
+WHERE LEN(Name_last)!=0
+AND LEN(Address_ZIP_1) !=0
+)
+
+UPDATE T1
+SET Assoc_HH_Rec = HH.HH_ID
+FROM AD_Customer.dbo.NPJXTCN_GOLDEN AS T1
+INNER JOIN HH ON T1.Cust_no=HH.Cust_no
+
+/** populate the remaining householding rows with their customer_number **/
+UPDATE AD_Customer.dbo.NPJXTCN_GOLDEN 
+SET Assoc_HH_Rec = Cust_no
+WHERE Assoc_HH_Rec IS NULL;
