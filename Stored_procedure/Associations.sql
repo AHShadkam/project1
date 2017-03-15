2d2d26ad5c15a3b2875a4f4eaa7ec81cb62bb5dc
AHShadkam <AmirHosein.Shadkam@searshc.com>
2017-03-15 11:56:43 -0500

    Update Associations.sql

--- Stored_procedure/Associations.sql
+++ Stored_procedure/Associations.sql
@@ -22,12 +22,12 @@ BEGIN
 
 -- create the Association columns
 ALTER TABLE [AD_Customer].[dbo].[NPJXTCN_GOLDEN]
-	ADD ( [Assoc_Ind_Rec] INT NULL,
-	      [Assoc_HH_Rec_1] INT NULL,
-          [Assoc_HH_Rec] INT NULL,
-          [Assoc_HH_Rec_1] INT NULL,
-          [Assoc_HH_Rec_2] INT NULL
-     )
+	ADD  [Assoc_Ind_Rec] INT NULL,
+	     [Assoc_Ind_Rec_1] INT NULL,
+             [Assoc_HH_Rec] INT NULL,
+             [Assoc_HH_Rec_1] INT NULL,
+             [Assoc_HH_Rec_2] INT NULL
+     
  
  
 -- house holding Name + Address --    
