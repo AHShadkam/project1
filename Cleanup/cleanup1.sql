9158543df674b95ee0322cfe3bcc819de61a9135
AHShadkam <AmirHosein.Shadkam@searshc.com>
2017-03-27 13:34:43 -0500

    Update cleanup1.sql

--- Cleanup/cleanup1.sql
+++ Cleanup/cleanup1.sql
@@ -98,18 +98,18 @@ GO
 
 -- 1.2) Remove leading non-alphabets from first-name 
 UPDATE [CustomerID].[dbo].[NPJXTCN_GOLDEN_NJ_AMIR] 
-SET [Name_first] = dbo.dbo.RemoveLeadingNonAlphabet([Name_first])
+SET [Name_first] = dbo.RemoveLeadingNonAlphabet([Name_first])
 WHERE [Name_first] LIKE '[^A-Z]%';
 GO
 -- 1.3) Remove trailing non-alphabets from first-name 
 UPDATE [CustomerID].[dbo].[NPJXTCN_GOLDEN_NJ_AMIR] 
-SET [Name_first] = dbo.dbo.RemoveTrailingNonAlphabet([Name_first])
+SET [Name_first] = dbo.RemoveTrailingNonAlphabet([Name_first])
 WHERE [Name_first] LIKE '%[^A-Z]';
 GO
 
 -- 1.4) Remove digits from first-name 
 UPDATE [CustomerID].[dbo].[NPJXTCN_GOLDEN_NJ_AMIR] 
-SET [Name_first] = dbo.dbo.RemoveDigitCharacters([Name_first])
+SET [Name_first] = dbo.RemoveDigitCharacters([Name_first])
 WHERE [Name_first] LIKE '%[0-9]%';
 GO
 
@@ -139,18 +139,18 @@ GO
 
 -- 2.2) Remove leading non-alphabets from last-name 
 UPDATE [CustomerID].[dbo].[NPJXTCN_GOLDEN_NJ_AMIR] 
-SET [Name_last] = dbo.dbo.RemoveLeadingNonAlphabet([Name_last])
+SET [Name_last] = dbo.RemoveLeadingNonAlphabet([Name_last])
 WHERE [Name_last] LIKE '[^A-Z]%';
 GO
 -- 2.3) Remove trailing non-alphabets from last-name 
 UPDATE [CustomerID].[dbo].[NPJXTCN_GOLDEN_NJ_AMIR] 
-SET [Name_last] = dbo.dbo.RemoveTrailingNonAlphabet([Name_last])
+SET [Name_last] = dbo.RemoveTrailingNonAlphabet([Name_last])
 WHERE [Name_last] LIKE '%[^A-Z]';
 GO
 
 -- 2.4) Remove digits from last-name 
 UPDATE [CustomerID].[dbo].[NPJXTCN_GOLDEN_NJ_AMIR] 
-SET [Name_last] = dbo.dbo.RemoveDigitCharacters([Name_last])
+SET [Name_last] = dbo.RemoveDigitCharacters([Name_last])
 WHERE [Name_last] LIKE '%[0-9]%';
 GO
 
