036e3bfec0e2812f016690e7d607f388755baeac
AHShadkam <AmirHosein.Shadkam@searshc.com>
2017-04-14 15:34:05 -0500

    Update Removeunwantedstring.sql

--- SQL on AWS/Functions/Removeunwantedstring.sql
+++ SQL on AWS/Functions/Removeunwantedstring.sql
@@ -1,22 +1,21 @@
 -- the ultimate function to clean last name and first name using other functions
 
-USE [CustomerID]
-GO;
+USE [CustomerID];
+GO
 
 IF OBJECT_ID('[dbo].[Removeunwantedstring]') IS NOT NULL
    DROP FUNCTION [dbo].[Removeunwantedstring];
 GO
-
 
 CREATE  Function [dbo].[Removeunwantedstring](@Temp VarChar(100))
 Returns VarChar(100)
 AS
 Begin
 	
-			Set @Temp = LTRIM(RTRIM(replace(replace(replace(
-						dbo.RemoveTrailingNonAlphabet(dbo.RemoveLeadingNonAlphabet(
-						replace(replace(replace(dbo.RemoveDigitCharacters(dbo.RemoveSpecialCharacters(
-						@Temp)),'MR.',''),'MS.',''),'MRS.',''))),' ','<>'),'><',''),'<>',' ')))
+	Set @Temp = LTRIM(RTRIM(replace(replace(replace(
+		dbo.RemoveTrailingNonAlphabet(dbo.RemoveLeadingNonAlphabet(
+		replace(replace(replace(dbo.RemoveDigitCharacters(dbo.RemoveSpecialCharacters(
+		@Temp)),'MR.',''),'MS.',''),'MRS.',''))),' ','<>'),'><',''),'<>',' ')))
 						
 	Return @Temp
 End;
